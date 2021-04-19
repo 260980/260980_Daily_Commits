42272acb66a83b9c851b9936343df65348a412b8
Narra Revathi <narrarevathi999@gmail.com>
2021-04-19 12:48:31 +0530

    Addition of two numbers from console

---
+++ Add two numbers_user input.py
@@ -0,0 +1,7 @@
+# Read input from console
+n1 = input('Enter first number : ')
+n2 = input('Enter second number : ')
+# Add two numbers
+sum = n1 + n2
+# Print the sum
+print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
